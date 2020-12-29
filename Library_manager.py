06a124bd0d4546ea7dc43ced07ee2eb3f354c48e
rnt434rnt <rntrnt434@gmail.com>
2020-12-29 18:51:03 +0330

    Add comments

--- Library_manager.py
+++ Library_manager.py
@@ -6,6 +6,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+# Window settings
+
 libraries = Tk()
 libraries.title('My Libraries')
 libraries.geometry('750x200')
@@ -19,6 +21,8 @@ list1.grid(row=0, column=0, rowspan=6, columnspan=2)
 
 sb1 = Scrollbar(libraries)
 sb1.grid(row=2, column=2, rowspan=6)
+
+# Sync listbox and scrollbar together
 
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
@@ -38,11 +42,11 @@ list1.bind('<<ListboxSelect>>', get_lib_details)
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-= Entries -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 e1 = Entry(libraries)
-e1.grid(row=4, column=4)
+e1.grid(row=4, column=3)
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-= Funcs -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-def listOfLibs():
+def listOfLibs(): # Update listbox and insert installed magules
     libs_names = check_output('pip list', shell=True)
     libs_names = libs_names.decode('utf-8')
     libs_names = libs_names.split('\n')
@@ -50,26 +54,32 @@ def listOfLibs():
     for lib in libs_names[2:-1:]:
         list1.insert(END, lib)
 
-def delete():
+def delete(): # Action for delete button. deletes selected magule
     call(f'pip uninstall {library_details[0]}', shell=True)
     time.sleep(1)
 
-def update():
+def update(): # Action for update button. updates selected magule
     call(f'pip install {library_details[0]} -U', shell=True)
 
-def install_lib():
+def install_lib(): # Action for Install button. Installs selected magule
     name = selected_lib
     url = f"https://pypi.org/search/?q={name}"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     result = soup.find_all("span", attrs={"class": "package-snippet__name"})
     call(f'pip install {result[0].text}', shell=True)
-    listOfLibs()
+    listOfLibs() # Listing new list
 
-def search_in_my_magules():
-    call(f'pip install {selected_lib}')
+def search_in_my_magules(): # Action for Search in my magules button. Lists list of installed maguls that e1.get() is in the magules name
+    libs_names = check_output('pip list', shell=True)
+    libs_names = libs_names.decode('utf-8')
+    libs_names = libs_names.split('\n')
+    list1.delete(0, END)
+    for lib in libs_names[2:-1:]:
+        if e1.get() in lib:
+            list1.insert(END, lib)
 
-def search_in_pypi():
+def search_in_pypi(): # Action for Search in Pypi button. Lists results of e1.get() search in pypi website. After this, appears install button
     url = f"https://pypi.org/search/?q={e1.get()}"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
@@ -78,6 +88,7 @@ def search_in_pypi():
     result = result[::-1]
     for magule in result:
         list1.insert(0, magule.text)
+    b4.grid(row=4, column=4)
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-= Buttons -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
@@ -91,16 +102,15 @@ b3 = Button(libraries, text='Update List', width=20, command=listOfLibs)
 b3.grid(row=1, column=3)
 
 b4 = Button(libraries, text='Install library', width=20, command=install_lib)
-b4.grid(row=3, column=3)
 
 b5 = Button(libraries, text='Exit', width=20, command=exit)
 b5.grid(row=1, column=4)
 
 b6 = Button(libraries, text='Search in my magules', width=20, command=search_in_my_magules)
-b6.grid(row=4, column=3)
+b6.grid(row=3, column=4)
 
 b7 = Button(libraries, text='Search in Pypi', width=20, command=search_in_pypi)
-b7.grid(row=3, column=4)
+b7.grid(row=3, column=3)
 
 listOfLibs()
 libraries.mainloop()
